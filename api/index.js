88f93a0300c496293e4ae1fb85ff873de7006201
ChamalHerath <chamal.j@coopinsu.com>
2023-10-28 23:56:36 +0530

    Signup Routes

--- api/index.js
+++ api/index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
+// DB Connection
 mongoose.connect(process.env.MONGODB_URL).then(() => {
     console.log("Connected to MongoDB!");
 }).catch((err) => {
@@ -12,8 +14,13 @@ mongoose.connect(process.env.MONGODB_URL).then(() => {
 
 const app = express();
 
+// Allow to use JSON
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log("Server is Running Port 3000...");
 })
 
+// Routes Manage
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
